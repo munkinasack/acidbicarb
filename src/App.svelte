d9a31f332b196bc726ce2741f339268ef91514f8
Jason <jason@lucastream.com>
2021-12-19 11:05:05 -0800

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Hoverable from './Hoverable.svelte';
+	let qrCode = '';
+</script>
+
+<Hoverable let:hovering={active}>
+	
+	<input class:active bind:value={qrCode} placeholder="Scan QR Code Here">
+
+</Hoverable>
+
+<style>
+	input {
+		padding: 1em;
+		margin: 0 0 1em 0;
+		background-color: #eee;
+	}
+
+	.active {
+		background-color: #ff3e00;
+		color: white;
+	}
+</style>
